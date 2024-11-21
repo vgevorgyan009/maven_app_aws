5540cf2998c13a2bab79c6493505949318b03151
vgevorgyan009 <vardan.gevorgyan.98@gmail.com>
2024-11-21 16:41:32 +0400

    Update script1.groovy

--- script1.groovy
+++ script1.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo 'building the docker image...'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t ${IMAGE_NAME} ."
+        sh 'docker build -t vgevorgyan009/demo-app:java-maven-5.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push ${IMAGE_NAME}"
+        sh 'docker push vgevorgyan009/demo-app:java-maven-5.0'
     }
 } 
 
